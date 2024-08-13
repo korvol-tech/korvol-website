d6f4002d56c60fd23a5c9f042bb84a61e8cd01d4
angular-slant <angularslant@gmail.com>
2024-08-13 21:01:14 +0530

    renamed to angularslant

--- runok/assets/js/slider.js
+++ runok/assets/js/slider.js
@@ -1,100 +1,103 @@
 (function ($) {
-    "use strict";
-    $(document).ready(function () {
-        function sliderAnimations(elements) {
-            var animationEndEvents = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
-            elements.each(function () {
-                var $this = $(this);
-                var $animationDelay = $this.data("delay");
-                var $animationDuration = $this.data("duration");
-                var $animationType = "runok-animation " + $this.data("animation");
-                $this.css({
-                    "animation-delay": $animationDelay,
-                    "-webkit-animation-delay": $animationDelay,
-                    "animation-duration": $animationDuration,
-                });
-                $this.addClass($animationType).one(animationEndEvents, function () {
-                    $this.removeClass($animationType);
-                });
-            });
-        }
-        var sliderOptions = {
-            speed: 1500,
-            autoplay: {
-                delay: 7000,
-            },
-            disableOnInteraction: false,
-            initialSlide: 0,
-            parallax: false,
-            mousewheel: false,
-            loop: true,
-            grabCursor: true,
-            effect: "fade",
-            navigation: {
-                nextEl: ".slider-arrow .slider-next",
-                prevEl: ".slider-arrow .slider-prev",
-            },
-            pagination: {
-                el: ".runok-swiper-pagination",
-                clickable: true,
-            },
-        };
-        sliderOptions.on = {
-            slideChangeTransitionStart: function () {
-                var swiper = this;
-                var animatingElements = $(swiper.slides[swiper.activeIndex]).find("[data-animation]");
-                sliderAnimations(animatingElements);
-            },
-
-            resize: function () {
-                this.update();
-            },
-        };
-
-        var swiper = new Swiper(".runok-slider-2", sliderOptions);
-
-
-        // Slider 1
-        
-        // Sliderthumb
-        var sliderThumb = new Swiper(".slider-tab", {
-            spaceBetween: 20,
-            slidesPerView: 3,
+  "use strict";
+  $(document).ready(function () {
+    function sliderAnimations(elements) {
+      var animationEndEvents =
+        "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+      elements.each(function () {
+        var $this = $(this);
+        var $animationDelay = $this.data("delay");
+        var $animationDuration = $this.data("duration");
+        var $animationType =
+          "angularSlant-animation " + $this.data("animation");
+        $this.css({
+          "animation-delay": $animationDelay,
+          "-webkit-animation-delay": $animationDelay,
+          "animation-duration": $animationDuration,
         });
-        var slider2Options = {
-            speed: 1500,
-            autoplay: {
-                delay: 7000,
-            },
-            disableOnInteraction: false,
-            initialSlide: 0,
-            parallax: false,
-            mousewheel: false,
-            loop: true,
-            grabCursor: true,
-            effect: "fade",
-            navigation: {
-                nextEl: ".slider-arrow .slider-next",
-                prevEl: ".slider-arrow .slider-prev",
-            },
-            thumbs: {
-                swiper: sliderThumb,
-            },
-        };
-        slider2Options.on = {
-            slideChangeTransitionStart: function () {
-                var swiper = this;
-                var animatingElements = $(swiper.slides[swiper.activeIndex]).find("[data-animation]");
-                sliderAnimations(animatingElements);
-            },
+        $this.addClass($animationType).one(animationEndEvents, function () {
+          $this.removeClass($animationType);
+        });
+      });
+    }
+    var sliderOptions = {
+      speed: 1500,
+      autoplay: {
+        delay: 7000,
+      },
+      disableOnInteraction: false,
+      initialSlide: 0,
+      parallax: false,
+      mousewheel: false,
+      loop: true,
+      grabCursor: true,
+      effect: "fade",
+      navigation: {
+        nextEl: ".slider-arrow .slider-next",
+        prevEl: ".slider-arrow .slider-prev",
+      },
+      pagination: {
+        el: ".angularSlant-swiper-pagination",
+        clickable: true,
+      },
+    };
+    sliderOptions.on = {
+      slideChangeTransitionStart: function () {
+        var swiper = this;
+        var animatingElements = $(swiper.slides[swiper.activeIndex]).find(
+          "[data-animation]"
+        );
+        sliderAnimations(animatingElements);
+      },
 
-            resize: function () {
-                this.update();
-            },
-        };
+      resize: function () {
+        this.update();
+      },
+    };
 
-        var swiper2 = new Swiper(".runok-slider", slider2Options);
+    var swiper = new Swiper(".angularSlant-slider-2", sliderOptions);
 
-        
+    // Slider 1
+
+    // Sliderthumb
+    var sliderThumb = new Swiper(".slider-tab", {
+      spaceBetween: 20,
+      slidesPerView: 3,
     });
+    var slider2Options = {
+      speed: 1500,
+      autoplay: {
+        delay: 7000,
+      },
+      disableOnInteraction: false,
+      initialSlide: 0,
+      parallax: false,
+      mousewheel: false,
+      loop: true,
+      grabCursor: true,
+      effect: "fade",
+      navigation: {
+        nextEl: ".slider-arrow .slider-next",
+        prevEl: ".slider-arrow .slider-prev",
+      },
+      thumbs: {
+        swiper: sliderThumb,
+      },
+    };
+    slider2Options.on = {
+      slideChangeTransitionStart: function () {
+        var swiper = this;
+        var animatingElements = $(swiper.slides[swiper.activeIndex]).find(
+          "[data-animation]"
+        );
+        sliderAnimations(animatingElements);
+      },
+
+      resize: function () {
+        this.update();
+      },
+    };
+
+    var swiper2 = new Swiper(".angularSlant-slider", slider2Options);
+  });
 })(jQuery);
