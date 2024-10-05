3bce83a9f0dcf8acf627e05664d7991aff6009ab
angular-slant <angularslant@gmail.com>
2024-10-05 19:20:35 +0530

    fixed lint isuses

--- src/page/home-landing/header/header.tsx
+++ src/page/home-landing/header/header.tsx
@@ -1,20 +1,6 @@
-import { FC, useState, useEffect } from "react";
+import { FC } from "react";
 
 const Header: FC = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsScrolled(window.scrollY > 0);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <header
       className={`w-full flex items-center px-2 md:px-8 h-16 fixed top-0 z-50 transition-colors duration-300"bg-black" 
